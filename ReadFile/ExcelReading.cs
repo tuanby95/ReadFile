7bd62902bf95fb39f2739d7a92308dc5502d2521
Tuan Duong <Tuan Duong@TOSZY>
2022-12-01 11:20:42 +0700

    Add project files.

---
+++ ReadFile/ExcelReading.cs
@@ -0,0 +1,30 @@
+﻿using IronXL;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFile
+{
+    public class ExcelReading
+    {
+        public void ReadExcelasJson()
+        {
+            var workBook = new WorkBook("ĐƯỜNG CF THỐNG NHẤT(2).xlsx");
+            var workSheet = workBook.GetWorkSheet("ĐƯỜNG");
+            var firstCell = workSheet.FirstFilledCell.AddressString;
+            var lastCell = workSheet.LastFilledCell.AddressString;
+            string _workSheetRange = $"{firstCell}:{lastCell}";
+            Console.WriteLine(_workSheetRange);
+            var range = workSheet.GetRange(_workSheetRange);
+            foreach(var cell in range )
+            {
+                Console.WriteLine(cell);
+            }
+            //return workBook.SaveAs("export.json");
+        }
+    }
+}
---
+++ ReadFile/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReadFile;
+
+string strDoc = @"D:\C#\Project\ReadFile\testdata.xlsx";
+var myExcelReading = new ExcelReading();
+myExcelReading.ReadExcelasJson();
