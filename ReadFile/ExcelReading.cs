9705dc4f25718603a5da7f37abcb85b023f09212
Tuan Duong Manh <dmtuan1995@gmail.com>
2022-12-01 15:47:39 +0700

    using bytescout package

--- ReadFile/ExcelReading.cs
+++ ReadFile/ExcelReading.cs
@@ -1,4 +1,6 @@
 ﻿using IronXL;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -6,25 +8,41 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bytescout.Spreadsheet;
 
 namespace ReadFile
 {
     public class ExcelReading
     {
-        public void ReadExcelasJson()
+        //public void ReadExcelasJson()
+        //{
+        //    var workBook = new WorkBook("ĐƯỜNG CF THỐNG NHẤT(2).xlsx");
+        //    var workSheet = workBook.GetWorkSheet("ĐƯỜNG");
+        //    var firstCell = workSheet.FirstFilledCell.AddressString;
+        //    var lastCell = workSheet.LastFilledCell.AddressString;
+        //    string _workSheetRange = $"{firstCell}:{lastCell}";
+        //    Console.WriteLine(_workSheetRange);
+        //    var range = workSheet.GetRange(_workSheetRange);
+        //    foreach(var cell in range )
+        //    {
+        //        Console.WriteLine(cell);
+        //    }
+        //    //return workBook.SaveAs("export.json");
+        //}
+        public void ReadExcel(string strDoc)
         {
-            var workBook = new WorkBook("ĐƯỜNG CF THỐNG NHẤT(2).xlsx");
-            var workSheet = workBook.GetWorkSheet("ĐƯỜNG");
-            var firstCell = workSheet.FirstFilledCell.AddressString;
-            var lastCell = workSheet.LastFilledCell.AddressString;
-            string _workSheetRange = $"{firstCell}:{lastCell}";
-            Console.WriteLine(_workSheetRange);
-            var range = workSheet.GetRange(_workSheetRange);
-            foreach(var cell in range )
+            Spreadsheet document = new Spreadsheet();
+            document.LoadFromFile(strDoc);
+            Worksheet worksheet = document.Workbook.Worksheets.ByName("ĐƯỜNG");
+            var rowCount = worksheet.Rows.LastFormatedRow;
+            var columnCount = worksheet.Columns.LastFormatedColumn;
+            for (int i = 0; i < rowCount; i++)
             {
-                Console.WriteLine(cell);
+                for (int j = 0; j < columnCount; j++)
+                {
+                    Console.WriteLine(worksheet.Cell(i, j));
+                }
             }
-            //return workBook.SaveAs("export.json");
         }
     }
 }
--- ReadFile/Program.cs
+++ ReadFile/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ReadFile;
 
-string strDoc = @"D:\C#\Project\ReadFile\testdata.xlsx";
+string strDoc = @"D:\C#\Project\ReadFile\ĐƯỜNG CF THỐNG NHẤT(2).xlsx";
 var myExcelReading = new ExcelReading();
-myExcelReading.ReadExcelasJson();
+myExcelReading.ReadExcel(strDoc);
